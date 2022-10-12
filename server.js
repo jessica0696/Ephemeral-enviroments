4bf19472922e05f5e0240811754e2f424486dada
Jessica Arambula <63938881+jessica0696@users.noreply.github.com>
2022-10-12 10:56:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,2 @@
-// Librerias y dependencias
-const http = require('http')
-const express = require('express')
-const app = express()
-const sqlite3 = require('sqlite3').verbose()
-const path = require('path')
-
-// Recursos
-app.use(express.static(__dirname + '/'))
 
 alert( '¡Hola, mundo!' );
